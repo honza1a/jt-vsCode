0fdb40a605630d2bd7011d5c779672e20f123520
honza1a <honza1313@gmail.com>
2021-04-26 10:22:58 +0200

    1

--- installGulp.cmd
+++ installGulp.cmd
@@ -1,4 +1,6 @@
-#source: https://gulpjs.com/docs/en/getting-started/quick-start/
+rem source: https://gulpjs.com/docs/en/getting-started/quick-start/
+cls
 node --version
+rem 
 npm --version
 npx --version
